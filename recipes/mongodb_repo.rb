aa6a54630d0b14be4c5312a3be7fd80dd7d90bae
Britt Treece <britt.treece@gmail.com>
2016-07-11 19:19:25 +0000

    add an end to our do

--- recipes/mongodb_repo.rb
+++ recipes/mongodb_repo.rb
@@ -7,7 +7,8 @@
 yum_repository 'mongodb-org-3.2' do
   description 'MongoDB Repository'
   baseurl 'https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/'
-   gpgcheck true
-   gpgkey 'https://www.mongodb.org/static/pgp/server-3.2.asc'
-   enabled true
-   action :create
+  gpgcheck true
+  gpgkey 'https://www.mongodb.org/static/pgp/server-3.2.asc'
+  enabled true
+  action :create
+end
