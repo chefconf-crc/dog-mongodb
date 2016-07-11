0baf755411c1233a1015a473bfda8ebc854dfc7c
Britt Treece <britt.treece@gmail.com>
2016-07-11 18:46:46 +0000

    fixed a spelling issue with the gpgkey line

--- recipes/mongodb_repo.rb
+++ recipes/mongodb_repo.rb
@@ -6,8 +6,8 @@
 
 yum_repository 'mongodb-org-3.2' do
   description 'MongoDB Repository'
-baseurl 'https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/'
-   gdpcheck tru
+  baseurl 'https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/'
+   gpgcheck true
    gpgkey 'https://www.mongodb.org/static/pgp/server-3.2.asc'
    enabled true
    action :create
